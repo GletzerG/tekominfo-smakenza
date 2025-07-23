7d0469b4b8ad07a5e0e5261bda2516602739d434
UmarSidiq08 <khairunisa.umar2004@gmail.com>
2025-07-23 22:10:30 +0800

    tes commmit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>ahahahahah</title>
+        <title>ahahahahhahahaah</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
