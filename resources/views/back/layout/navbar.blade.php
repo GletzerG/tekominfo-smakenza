b6c154d983efc71c729442f54d37ba297810012c
Gletzer Julio Ivanees <gletzerg@gmail.com>
2025-07-28 21:00:59 +0800

    lupa apa namanya

--- resources/views/back/layout/navbar.blade.php
+++ resources/views/back/layout/navbar.blade.php
@@ -135,6 +135,10 @@
                             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                             Logout
                         </a>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                            <i class="bi bi-sign-intersection fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
                     </div>
                 </li>
 
